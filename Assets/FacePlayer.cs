2dd9f5e677cf7d532bb685e8f563058765fc915a
Nguyen Van <nvan@upei.ca>
2016-04-14 20:54:15 -0300

    Crocodile

---
+++ Assets/FacePlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FacePlayer : MonoBehaviour {
+
+    public Transform playerPos;
+    public bool facingLeft;
+
+    // Use this for initialization
+    void Start () {
+        facingLeft = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (playerPos.position.x < gameObject.transform.position.x && !facingLeft )
+        {
+            facingLeft = true;
+            Invoke("Flip", 0.5f);
+        }
+        else if (playerPos.position.x > gameObject.transform.position.x && facingLeft)
+        {
+            facingLeft = false;
+            Invoke("Flip", 0.5f);
+        }
+	}
+
+    void Flip()
+    {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
